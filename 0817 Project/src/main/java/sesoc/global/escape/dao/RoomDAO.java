ee6876f109b7ead7457d1f046ee5bc399c7977f8
gutorEunji <vktmffl6790@gmail.com>
2017-09-05 17:33:03 +0900

    오타 수정

--- 0817 Project/src/main/java/sesoc/global/escape/dao/RoomDAO.java
+++ 0817 Project/src/main/java/sesoc/global/escape/dao/RoomDAO.java
@@ -5,6 +5,6 @@ import sesoc.global.escape.vo.Room;
 public interface RoomDAO {
 	public int insertRoom(Room room);
 	public int deleteRoom(Room room);
-	public int insertWatingUser(Room room);
+	public int insertWaitingUser(Room room);
 	public int deleteWaitingUser(Room room);
 }
