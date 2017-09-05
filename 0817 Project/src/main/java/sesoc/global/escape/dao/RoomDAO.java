516b0757c17c0481d7b69fc67230b60f82bd3864
gutorEunji <vktmffl6790@gmail.com>
2017-09-05 17:20:04 +0900

    dao 외 몇가지 test

---
+++ 0817 Project/src/main/java/sesoc/global/escape/dao/RoomDAO.java
@@ -0,0 +1,10 @@
+package sesoc.global.escape.dao;
+
+import sesoc.global.escape.vo.Room;
+
+public interface RoomDAO {
+	public int insertRoom(Room room);
+	public int deleteRoom(Room room);
+	public int insertWatingUser(Room room);
+	public int deleteWaitingUser(Room room);
+}
---
+++ 0817 Project/src/main/java/sesoc/global/escape/repository/RoomRepository.java
@@ -0,0 +1,35 @@
+package sesoc.global.escape.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import sesoc.global.escape.dao.RoomDAO;
+import sesoc.global.escape.vo.Room;
+
+@Repository
+public class RoomRepository {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	public int insertRoom(Room room) {
+		RoomDAO dao = sqlSession.getMapper(RoomDAO.class);
+		return dao.insertRoom(room);
+	}
+	
+	public int deleteRoom(Room room) {
+		RoomDAO dao = sqlSession.getMapper(RoomDAO.class);
+		return dao.deleteRoom(room);
+	}
+	
+	public int insertWatingUser(Room room) {
+		RoomDAO dao = sqlSession.getMapper(RoomDAO.class);
+		return dao.insertWatingUser(room);
+	}
+	
+	public int deleteWatingUser(Room room) {
+		RoomDAO dao = sqlSession.getMapper(RoomDAO.class);
+		return dao.deleteWaitingUser(room);
+	}
+}
